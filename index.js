71b69cd4d3f1d543137541331fee4419ca0d8108
Ikechukwu <iykekings36@gmail.com>
2019-07-08 13:10:19 +0100

    create get all users endpoint

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const server = require('express')();
 const Users = require('./data/db');
 const PORT = 3000;
 
+//fetch all users from db
 server.get('/api/users', (reg, res) => {
-  res.json('Working');
+  Users.find()
+    .then(data => {
+      if (data.length) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json('There are no users');
+      }
+    })
+    .catch(err => {
+      res.status(500).json(`Error fetching posts: ${err}`);
+    });
 });
 
 server.listen(3000, () => {
