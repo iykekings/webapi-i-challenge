7ddb16cdb42cd8953f2d6f02914ef089f3270151
Ikechukwu <iykekings36@gmail.com>
2019-07-08 13:01:36 +0100

    Installed express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+const server = require('express')();
+const Users = require('./data/db');
+const PORT = 3000;
+
+server.get('/api/users', (reg, res) => {
+  res.json('Working');
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost/api/users/${PORT}`);
+});
